10a6dfa80e91b87c2ecff688783f6f167e8e94ae
owcschafer <owcschafer@hotmail.com>
2020-11-29 09:55:35 -0800

    san's files

--- main.py
+++ main.py
@@ -1,44 +1,20 @@
 from flask import Flask, render_template, url_for, flash
 from forms import RegistrationForm, LoginForm
+import save_data as sd
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '6e10fe750c13a1e1220a9cc63307664c'
 
-posts = [
-    {
-    'author': 'Corey Schafer',
-    'title': 'Blog Post 1',
-    'content': 'First post content',
-    'date_posted': 'April 20, 2018'
-    },
-    {
-    'author': 'Jane Doe',
-    'title': 'Blog Post 2',
-    'content': 'Second post content',
-    'date_posted': 'April 21, 2018'
-    }
-]
+courses_list = sd.get_courses_json()
 
 @app.route("/")
-@app.route("/home")
-def home():
-    return render_template('home.html', posts=posts)
+@app.route("/courses")
+def courses():
+    return render_template('courses.html', courses_list=courses_list)
 
-@app.route("/about")
-def about():
-    return render_template('about.html', title='About')
-
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-  form = RegistrationForm()
-  if form.validate_on_submit():
-    flash(f'Account created for {form.username.data}!')
-  return render_template('register.html', title='Register', form=form)
-
-@app.route("/login")
-def login():
-  form = LoginForm()
-  return render_template('login.html', title='Login', form=form)
+@app.route("/assignments")
+def assignments():
+    return render_template('assignments.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ script.js
@@ -0,0 +1,62 @@
+function sub()
+{
+ var an = document.getElementById('AssignmentName').value;
+ var dd = document.getElementById('DueDate').value;
+ var al = document.getElementById('AssignmentLink').value;
+ var ac = document.getElementById('AssignmentClass').value;
+ var td = document.getElementById('dropdown').value;
+
+if (an.length == 0){
+  alert("Assignment Name is required")
+}
+else {
+
+if (al.length > 0){
+  linkHtml = "<a href=" + al + " target=\"_blank\">"
+}
+else {
+  linkHtml = ""
+}
+
+if (dd != "" && ac != ""){
+  hoverText = dd + " - " + ac
+}
+else {
+  hoverText = dd + ac
+}
+var table = document.getElementById("table");
+var rows = table.rows.length;
+var row = table.insertRow(rows);
+
+if (td == "To Do") {
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  cell1.innerHTML = "<div ondblclick=\"del()\" class=\"myDIV\">" + linkHtml + an + "</a></div> <div class=\"hide\">" + hoverText + "</div>";
+}
+else if (td == "Doing") {
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  cell2.innerHTML = "<div ondblclick=\"del()\" class=\"myDIV\">" + linkHtml + an + "</a></div> <div class=\"hide\">" + hoverText + "</div>";
+}
+else {
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  cell3.innerHTML = "<div ondblclick=\"del()\" class=\"myDIV\">" + linkHtml + an + "</a></div> <div class=\"hide\">" + hoverText + "</div>";
+}
+}
+}
+function del() {
+
+$("table tr").not("thead tr").unbind().dblclick(function(){
+    clickedRow = this.rowIndex
+    if (clickedRow == 0) {
+      null
+    }
+    else {
+    table.deleteRow(clickedRow);
+    }
+});
+}
